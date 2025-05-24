0f0a12b7e7aa976068e400a6c812ef621be9111d
Marcelo Jhones <120574951+MarceloJFF@users.noreply.github.com>
2025-05-24 09:05:41 -0300

    Update axios.config.js

--- recycle/src/services/axios.config.js
+++ recycle/src/services/axios.config.js
@@ -4,8 +4,10 @@ import { useAuthStore } from '@/stores/auth';
 // Configuração base do axios
 const axiosInstance = axios.create({
   baseURL: 'https://trilhas-desafio-4-production-890a.up.railway.app/', // ⬅️ troque pela URL da sua API
+  withCredentials: false, // Mude para true se usar cookies/sessões
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
   }
 });
 export default axiosInstance;
