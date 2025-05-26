89b655443ee6a9d83cb0311f33e69cee23024a80
Marcelo Jhones <120574951+MarceloJFF@users.noreply.github.com>
2025-05-26 14:39:31 -0300

    Update vite.config.ts

--- recycle/vite.config.ts
+++ recycle/vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // 👈 IMPORTANTE para deploy no domínio raiz
   plugins: [
     vue(),
     vueDevTools(),
